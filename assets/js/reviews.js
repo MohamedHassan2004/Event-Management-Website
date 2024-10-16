50e21278ccecfe0ecdcf27e485167d96aaf143ab
MohamedHassan2004 <147160046+MohamedHassan2004@users.noreply.github.com>
2024-10-16 18:11:46 +0300

    make elements more responsive

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -9,18 +9,29 @@ var i = 0;
 let l = document.querySelector("#l");
 let r = document.querySelector("#r");
 
-
-changeContent("+");
-showReview();
-
-l.addEventListener('click', ()=> changeContent("-"));
-r.addEventListener('click',()=>changeContent("+"));
-/*********************************************************************8 */
-function showReview(){
+if(array.length == 0){
+    let noReviewsP = document.createElement('p');
+    noReviewsP.innerHTML = "There's No Reviews Yet";
+    noReviewsP.className = "text-center";
+    reviewRow.appendChild(noReviewsP);
+    l.style.display = "none";
+    r.style.display = "none";   
+}else if (array.length == 1){
     changeContent("+");
-    i = (i + 1) % array.length
-    setTimeout("showReview()",5000);
+    l.style.display = "none";
+    r.style.display = "none"; 
 }
+else{
+    changeContent("+");
+    l.addEventListener('click', ()=> changeContent("-"));
+    r.addEventListener('click',()=>changeContent("+"));
+}
+
+let reviewsNo = document.querySelector('#reviews-no');
+reviewsNo.innerHTML = array.length;
+reviewsNo.style.color = "#0d6efd";
+
+/**********************************************************************/
 
 function changeContent(sign){
     reviewRow.innerHTML = "";
@@ -30,5 +41,17 @@ function changeContent(sign){
         if(i == 0) i = array.length;
         i = (i - 1);
     }
-    reviewRow.appendChild(array[i]);
+    reviewRow.appendChild(justifiedReview(array[i]));
+}
+
+function justifiedReview(x){
+    maxLen = 400;
+    p = x.querySelector('p');
+    tmp = p.textContent;
+    if(tmp.length > maxLen){
+        p.innerHTML = `${tmp.slice(0,maxLen)}...<span class = "read-more">read more</span>`;
+        let readmore = p.querySelector('.read-more');
+        readmore.addEventListener('click',()=> p.innerHTML = tmp);
+    }
+    return x;
 }
